b0221caaebfef0bf4385ed455d213e032e58dae8
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-21 18:37:35 -0700

    added writefile and new prompt functions

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+const inquirer = require('inquirer');
 const fs = require('fs');
 
-const prompt = [
+const prompts = [
     {
         type: 'input',
         name: 'name',
@@ -23,3 +24,51 @@ const prompt = [
     }
 ]
 
+// Functions //
+// to generate html
+const writeToFile = (fileName, teamData) =>{
+    return new Promise((Res, rej) =>{
+        fs.writeFile('./dist/team.html'), (fileName, teamData), err =>{
+            if(err){
+                rej(err);
+                return;
+            }
+            Res({
+                ok: true,
+                message: 'Success! You have created the html.'
+            })
+        }
+    })
+}
+
+// adding a new employee
+const addNewPrompt = () =>{
+    inquirer.prompt(
+        {
+            type: 'confirm',
+            name: 'addConfirm',
+            message: "Would you like to add another employee?"
+        }
+    )
+    .then(response => {
+        if(response.addConfirm){
+            inquirer.prompt({
+                type: 'list',
+                name: 'role',
+                message: "What's the employee's role?",
+                choices: ['Engineer', 'Intern']
+            })
+            .then(response =>{
+                if(response.role === 'Engineer'){
+                    promptEngineer();
+                }else{
+                    promptIntern();
+                }
+            })
+        }else{
+            console.log(teamData)
+            let html = generatePage(teamData)
+            writeToFile('team.html', html);
+        }
+    })
+}
